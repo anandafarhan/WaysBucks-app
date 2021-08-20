a5fddc55d3fe824bbc8ff17696671e22340605c1
Ananda Farhan <nandafarhan@gmail.com>
2021-08-20 10:25:21 +0700

    Login using data from localStorage

--- src/components/Modal/LoginModal.js
+++ src/components/Modal/LoginModal.js
@@ -1,30 +1,93 @@
-import React from 'react'
-import {Modal, Button, Form} from 'react-bootstrap'
+import React, { useState } from 'react';
+import { Modal, Button, Form, Alert } from 'react-bootstrap';
 
 function LoginModal(props) {
-   return (
-      <Modal show={props.show} onHide={props.handleClose} dialogClassName="modal-overide" centered>
-        <Modal.Body>
-        <Modal.Title className="text-overide"><strong>Login</strong></Modal.Title>
-        <br/>
-         <Form>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-               <Form.Control type="email" placeholder="Email" className="input-overide" />
-            </Form.Group>
+	const [formData, setFormData] = useState({
+		email: '',
+		password: '',
+	});
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-               <Form.Control type="password" placeholder="Password" className="input-overide" />
-            </Form.Group>
-            <div className="d-grid gap-2 my-3">
-            <Button variant="danger" className="bg-overide" type="submit">
-                  Login
-               </Button>
-            </div>
-              <p className="text-center">Don't have an account ? Click <span onClick={props.switchModal}><strong>Here</strong></span></p>
-            </Form>
-         </Modal.Body>
-      </Modal>
-   )
+	const handleChange = (e) => {
+		setFormData({ ...formData, [e.target.name]: e.target.value });
+	};
+
+	const [wrongCred, setCred] = useState(false);
+
+	function handleSubmit(e) {
+		e.preventDefault();
+		const email = window.localStorage.getItem('email');
+		const password = window.localStorage.getItem('password');
+
+		const success = () => {
+			window.localStorage.setItem('isLogedIn', true);
+			props.handleClose();
+		};
+		const fail = () => {
+			setCred(true);
+			setFormData({
+				password: '',
+			});
+		};
+
+		email === formData.email && password === formData.password
+			? success()
+			: fail(e);
+	}
+	return (
+		<Modal
+			show={props.show}
+			onHide={props.handleClose}
+			dialogClassName='modal-overide'
+			centered
+		>
+			<Modal.Body>
+				<Modal.Title className='text-overide'>
+					<strong>Login</strong>
+				</Modal.Title>
+				<br />
+				<Form onSubmit={(e) => handleSubmit(e)}>
+					<Form.Group className='mb-3' controlId='Email'>
+						<Form.Control
+							type='email'
+							placeholder='Email'
+							name='email'
+							className='input-overide'
+							value={formData.email}
+							onChange={(e) => handleChange(e)}
+						/>
+					</Form.Group>
+
+					<Form.Group className='mb-3' controlId='Password'>
+						<Form.Control
+							type='password'
+							placeholder='Password'
+							name='password'
+							className='input-overide'
+							value={formData.password}
+							onChange={(e) => handleChange(e)}
+						/>
+					</Form.Group>
+					<div className='d-grid gap-2 my-3'>
+						<Button variant='danger' className='bg-overide' type='submit'>
+							Login
+						</Button>
+					</div>
+					<Alert
+						variant='danger'
+						style={wrongCred ? { display: 'block' } : { display: 'none' }}
+					>
+						Wrong email and password combination
+					</Alert>
+					<p className='text-center'>
+						Don't have an account ? Click{' '}
+						<span onClick={props.switchModal}>
+							<strong>Here</strong>
+						</span>
+					</p>
+				</Form>
+			</Modal.Body>
+		</Modal>
+	);
 }
 
-export default LoginModal
+export default LoginModal;
--- src/components/Navbar/Headers.js
+++ src/components/Navbar/Headers.js
@@ -1,62 +1,128 @@
-import React,  {useReducer} from 'react'
-import {Navbar, Nav, Container, Button} from 'react-bootstrap'
-import Logo from '../../assets/Logo.svg'
+import React, { useReducer } from 'react';
+import { useHistory } from 'react-router';
+import { Navbar, Nav, Container, Button, Dropdown } from 'react-bootstrap';
+import { BiCartAlt, BiUserCircle, BiLogOutCircle } from 'react-icons/bi';
+import Logo from '../../assets/Logo.svg';
+import Avatar from '../../assets/Avatar.svg';
 import LoginModal from '../Modal/LoginModal';
-import RegisterModal from '../Modal/RegisterModal';
+// import RegisterModal from '../Modal/RegisterModal';
 
 function Headers() {
-   const initialState = {
-      modalLogin: false,
-      modalRegister: false
-   };
+	const route = useHistory();
 
-   function reducer(state, action) {
-      switch (action.type) {
-        case 'showModalL':
-          return {modalLogin: true};
-        case 'showModalR':
-          return {modalRegister: true};
-        case 'hideModalL':
-          return {modalLogin: false};
-        case 'hideModalR':
-          return {modalRegister: false};
-        case 'switchModal':
-          return {
-             modalRegister: !state.modalRegister,
-             modalLogin: !state.modalLogin,
-         };
-        default:
-          throw new Error();
-      }
-    }
+	const initialState = {
+		modalLogin: false,
+		modalRegister: false,
+	};
 
-   const [state, dispatch] = useReducer(reducer, initialState);
-//    const [showLogin, setShowL] = useState(false);
-//    const [showRegister, setShowR] = useState(false);
+	function reducer(state, action) {
+		switch (action.type) {
+			case 'showModalL':
+				return { modalLogin: true };
+			case 'showModalR':
+				return { modalRegister: true };
+			case 'hideModalL':
+				return { modalLogin: false };
+			case 'hideModalR':
+				return { modalRegister: false };
+			case 'forceRender':
+				return {};
+			case 'switchModal':
+				return {
+					modalRegister: !state.modalRegister,
+					modalLogin: !state.modalLogin,
+				};
+			default:
+				throw new Error();
+		}
+	}
 
-//   const handleCloseL = () => setShowL(false);
-//   const handleShowL = () => setShowL(true);
-//   const handleCloseR = () => setShowR(false);
-//   const handleShowR = () => setShowR(true);
-   return (
-      <>
-      <Navbar collapseOnSelect expand="lg">
-         <Container>
-            <Navbar.Brand href="#home"><img src={Logo} alt="..." width="60px" /></Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-               <Nav className="me-auto">
-               </Nav>
-               <Nav>
-                  <Nav.Link><Button variant="outline-danger" size="sm" onClick={()=>dispatch({type:'showModalL'})}>Login</Button></Nav.Link>
-                  <Nav.Link><Button variant="danger" size="sm" className="bg-overide" onClick={()=>dispatch({type:'showModalR'})}>Register</Button></Nav.Link>
-               </Nav>
-            </Navbar.Collapse>
-         </Container>
-      </Navbar>
-      <LoginModal handleClose={()=>dispatch({type:'hideModalL'})} switchModal={()=>dispatch({type:'switchModal'})} show={state.modalLogin} />
-      </>
-   )
+	const [state, dispatch] = useReducer(reducer, initialState);
+	const isLogedIn = window.localStorage.getItem('isLogedIn');
+
+	function handleLogout() {
+		window.localStorage.setItem('isLogedIn', false);
+		dispatch({ type: 'forceRender' });
+		route.push('/');
+	}
+
+	const guest = (
+		<>
+			<Nav.Link>
+				<Button
+					variant='outline-danger'
+					size='sm'
+					onClick={() => dispatch({ type: 'showModalL' })}
+				>
+					Login
+				</Button>
+			</Nav.Link>
+			<Nav.Link>
+				<Button
+					variant='danger'
+					size='sm'
+					className='bg-overide'
+					onClick={() => dispatch({ type: 'showModalR' })}
+				>
+					Register
+				</Button>
+			</Nav.Link>
+		</>
+	);
+	const user = (
+		<>
+			<BiCartAlt className='mx-3 my-auto icons-img' size='3rem' />
+			<Dropdown as={Nav.Item} className='ml-3'>
+				<Dropdown.Toggle as={Nav.Link}>
+					<img
+						className=''
+						src={Avatar}
+						alt='user pic'
+						width='50px'
+						style={{ position: 'relative', transform: 'translate(0,0)' }}
+					/>
+				</Dropdown.Toggle>
+				<Dropdown.Menu align='right'>
+					<Dropdown.Item>
+						<BiUserCircle className='mx-2 icons-img' size='2rem' />
+						Profile
+					</Dropdown.Item>
+					<Dropdown.Item onClick={handleLogout}>
+						<BiLogOutCircle className='mx-2 icons-img' size='2rem' />
+						Logout
+					</Dropdown.Item>
+				</Dropdown.Menu>
+			</Dropdown>
+		</>
+	);
+	//    const [showLogin, setShowL] = useState(false);
+	//    const [showRegister, setShowR] = useState(false);
+
+	//   const handleCloseL = () => setShowL(false);
+	//   const handleShowL = () => setShowL(true);
+	//   const handleCloseR = () => setShowR(false);
+	//   const handleShowR = () => setShowR(true);
+	return (
+		<>
+			<Navbar collapseOnSelect expand='lg'>
+				<Container>
+					<Navbar.Brand href='#home'>
+						<img src={Logo} alt='...' width='70px' />
+					</Navbar.Brand>
+					<Navbar.Toggle aria-controls='responsive-navbar-nav' />
+					<Navbar.Collapse id='responsive-navbar-nav'>
+						<Nav className='me-auto'></Nav>
+						<Nav>{isLogedIn === 'true' ? user : guest}</Nav>
+					</Navbar.Collapse>
+				</Container>
+			</Navbar>
+			<LoginModal
+				handleClose={() => dispatch({ type: 'hideModalL' })}
+				switchModal={() => dispatch({ type: 'switchModal' })}
+				show={state.modalLogin}
+			/>
+		</>
+	);
 }
 
-export default Headers
+export default Headers;
