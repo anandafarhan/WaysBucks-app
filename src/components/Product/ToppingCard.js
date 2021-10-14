69515a14874ecbe66154787808be895fc747cb7b
Ananda Farhan <nandafarhan@gmail.com>
2021-10-14 17:48:47 +0700

    Temporary fix checked topping style

--- src/components/Product/ToppingCard.js
+++ src/components/Product/ToppingCard.js
@@ -1,10 +1,24 @@
+import { useEffect, useState } from 'react';
 import { Card } from 'react-bootstrap';
 
 function ToppingCard({ ...props }) {
 	const { id, name, image } = props.topping;
+	const [checked, setChecked] = useState(false);
+
+	useEffect(() => {
+		if (props.checked) {
+			setChecked(true);
+		} else {
+			setChecked(false);
+		}
+	}, [props.checked]);
 
 	return (
-		<Card border='white' className='my-1 grow'>
+		<Card
+			border='white'
+			className='my-1'
+			style={{ filter: checked ? 'drop-shadow(0 0 2px #dddddd)' : 'drop-shadow(0 0 0 #dddddd)' }}
+		>
 			<Card.Body>
 				<label className='block-check'>
 					<img src={image} alt={name} className='img-topping' />
@@ -18,7 +32,10 @@ function ToppingCard({ ...props }) {
 					<span className='checkmark'></span>
 				</label>
 			</Card.Body>
-			<Card.Text className='fs-6 fw-normal text-center text-nowrap text-overide'>
+			<Card.Text
+				className='fw-normal text-center text-nowrap text-overide'
+				style={{ fontSize: '.9rem' }}
+			>
 				<span>{name}</span>
 			</Card.Text>
 		</Card>
